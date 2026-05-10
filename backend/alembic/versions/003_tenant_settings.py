67e13c17fa98c43487a814966ace06397b44eb2d
Ubuntu <ubuntu@ip-172-31-42-42.ec2.internal>
2026-05-10 19:03:49 +0000

    fix: stub 003 para alinhar com banco

---
+++ backend/alembic/versions/003_tenant_settings.py
@@ -0,0 +1,12 @@
+"""tenant_settings - stub para alinhar com banco
+
+Revision ID: 003_tenant_settings
+Revises: 002_add_pix_receipt_logs
+"""
+from alembic import op
+revision = '003_tenant_settings'
+down_revision = '002_add_pix_receipt_logs'
+branch_labels = None
+depends_on = None
+def upgrade(): pass
+def downgrade(): pass
